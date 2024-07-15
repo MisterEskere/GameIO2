e35a48ce25ed9f3b44940653bd852a1c6dbd1add
MisterEskere <mistereskere@protonmail.com>
2024-07-15 21:04:54 +0200

    chore: Update get_api_key function to handle missing API_KEY in .env file

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,8 +1,4 @@
-use std::env;
-use dotenv::dotenv;
 use serde_json::Value;
-use std::fs::OpenOptions;
-use std::path::Path;
 
 pub async fn get_request(url: &str) -> Result<Value, reqwest::Error> {
     let client = reqwest::Client::new();
@@ -15,27 +11,54 @@ pub async fn get_request(url: &str) -> Result<Value, reqwest::Error> {
 }
 
 pub async fn get_api_key() -> String {
-    // Check if the .env file exists, create it if it doesn't
-    let env_path = Path::new(".env");
-    if !env_path.exists() {
-        let _ = OpenOptions::new().create(true).write(true).open(env_path);
-    }
+    
+    // Create the database.sqlite file
+    create_database_sqlite().await;
 
-    // Retrieve the API_KEY from the .env file
-    dotenv().ok();
-    let api_key = env::var("API_KEY").expect("API_KEY must be set!!!");
+    // query the database for the API_KEY
+    let conn = rusqlite::Connection::open("database.sqlite").unwrap();
+    let api_key: String = conn.query_row(
+        "SELECT api_key FROM settings LIMIT 1",
+        [],
+        |row| row.get(0),
+    ).unwrap();
+
+    // Return the API_KEY
     api_key
 }
 
 pub async fn set_api_key(api_key: &str) {
 
-    // Check if the .env file exists, create it if it doesn't
-    let env_path = Path::new(".env");
-    if !env_path.exists() {
-        let _ = OpenOptions::new().create(true).write(true).open(env_path);
-    }
+    // Create the database.sqlite file
+    create_database_sqlite().await;
 
-    // Set the API_KEY in the .env file
-    dotenv().ok();
-    env::set_var("API_KEY", api_key);
+    // Update the database with the new API_KEY
+    let conn = rusqlite::Connection::open("database.sqlite").unwrap();
+    conn.execute(
+        "INSERT OR REPLACE INTO settings (api_key) VALUES (?)",
+        [api_key],
+    ).unwrap();
+
+}
+
+
+
+/// Function to create a database.sqlite file. This will be used for persistent storage.
+/// Table: Settings (1 row)
+/// Columns: api_key TEXT
+async fn create_database_sqlite() {
+    // Check if the database file already exists
+    if !std::path::Path::new("database.sqlite").exists() {
+        // Create the database file
+        let conn = rusqlite::Connection::open("database.sqlite").unwrap();
+
+        // Create the settings table
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS settings (
+                api_key TEXT
+            )",
+            [],
+        )
+        .unwrap();
+    }
 }
