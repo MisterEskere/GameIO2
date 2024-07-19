56ff081afef660dcfb9f7a3099bda24996fc72b8
MisterEskere <mistereskere@protonmail.com>
2024-07-19 19:27:18 +0200

    chore: Update dirs dependency to version 5.0.1

--- src-tauri/src/env.rs
+++ src-tauri/src/env.rs
@@ -1,7 +1,6 @@
 use serde_json::Value;
-use std::io::Write;
 use std::env::VarError;
-
+use std::io::Write;
 
 /// Function to make a GET request to a URL and return the JSON response.
 pub async fn get_request(url: &str) -> Result<Value, reqwest::Error> {
@@ -45,7 +44,6 @@ pub async fn set_download_path(download_path: &str) -> Result<(), std::io::Error
 
 /// Function to create the .ENV file with the API_KEY and the DOWNLOAD_PATH.
 fn create_env_file() -> Result<(), std::io::Error> {
-
     // Attempt to create the .env file, in case it already exists exit early
     let mut file = std::fs::OpenOptions::new()
         .write(true)
@@ -55,11 +53,15 @@ fn create_env_file() -> Result<(), std::io::Error> {
     // Get the API_KEY from the environment
     let api_key = std::env::var("API_KEY").unwrap();
 
-    // Get the default download path from the user's directory
-    let download_path = dirs::download_dir().unwrap().to_str().unwrap();
+    // Store the PathBuf in a variable
+    let download_path_buf = dirs::download_dir().unwrap();
+
+    // Convert the PathBuf to a &str, ensuring the PathBuf lives long enough
+    let download_path = download_path_buf.to_str().unwrap();
 
     // Write the API_KEY and DOWNLOAD_PATH to the file
-    file.write_all(b"API_KEY=YOUR_API_KEY\nDOWNLOAD_PATH=YOUR_DOWNLOAD_PATH")?;
+    file.write_all(format!("API_KEY={}\n", api_key).as_bytes())?;
+    file.write_all(format!("DOWNLOAD_PATH={}", download_path).as_bytes())?;
 
     Ok(())
 }
