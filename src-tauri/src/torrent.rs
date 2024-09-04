96f87ae28722956675bef74f04c413788d5691e9
MisterEskere <your_email>
2024-09-05 00:26:31 +0200

    added tests for torrents

--- src-tauri/src/torrent.rs
+++ src-tauri/src/torrent.rs
@@ -28,7 +28,6 @@ static HANDLES: Lazy<Arc<Mutex<Vec<Arc<ManagedTorrent>>>>> =
 ///
 pub async fn download_torrent(directory: &str, magnet_link: &str) {
 
-    print!("Downloading torrent... {}", magnet_link);
     // Create the session
     trace!("Creating session");
     debug!("Directory: {}", directory);
@@ -112,6 +111,7 @@ pub async fn get_torrent_statuses() -> Vec<std::string::String> {
     // Create the result vector
     let mut statuses = Vec::new();
 
+
     // Iterate over all the handles
     for handle in handles.iter() {
 
@@ -122,9 +122,35 @@ pub async fn get_torrent_statuses() -> Vec<std::string::String> {
         statuses.push(status.to_string());
     }
 
+    print!("Number of torrents: {}", statuses.len());
     debug!("Number of torrents: {}", statuses.len());
 
     // Return the result vector
     statuses
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_download_torrent() {
+        let magnet_link = "magnet:?xt=urn:btih:8DF6E26142615621983763B729F640372CF1FC34&dn=Linux+Mint+20.1+%26quot%3BUlyssa%26quot%3B+-+Cinnamon+%2864-bit%29&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce";
+        download_torrent("/home/user/Downloads", magnet_link).await;
+
+        // Test will pass if no error is thrown during the download of a torrent via magnetlink
+        // This might take a while to complete
+        // Linux mint is used at its well seeded
+        assert!(true);
+    }
+
+    #[tokio::test]
+    async fn test_get_torrent_statuses() {
+        let magnet_link = "magnet:?xt=urn:btih:8DF6E26142615621983763B729F640372CF1FC34&dn=Linux+Mint+20.1+%26quot%3BUlyssa%26quot%3B+-+Cinnamon+%2864-bit%29&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce";
+        download_torrent("/home/user/Downloads", magnet_link).await;
+
+        let statuses = get_torrent_statuses().await;
+
+        assert!(statuses.len() == 1)
+    }
+}
